186196c5ac20549f2265b0470eedb1d02144790f
Andreas Eliason <andreashg.eliasson@gmail.com>
2016-12-02 10:42:45 +0000

    modify toy example to return json

--- app/water_fp.py
+++ app/water_fp.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, Response
+import json
 app = Flask(__name__)
 
 @app.route('/')
 def load_page():
-    return 'Water footprint page goes here!'
+    data = json.dumps([{"tomato": 1.12}, {"potato": 2.12}])
+    resp = Response(response=data,
+                    status=200,
+                    mimetype="application/json")
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
