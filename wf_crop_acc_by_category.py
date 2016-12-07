b64ed4b409fabc324302acfd325b9a916430b1c1
Andreas Eliason <andreashg.eliasson@gmail.com>
2016-12-07 15:46:58 +0000

    collect aggregated wf values for each ingredient

--- wf_crop_acc_by_category.py
+++ wf_crop_acc_by_category.py
@@ -24,7 +24,6 @@ current_category_acc = {
 
 for doc in collection.find():
     if doc["product_category"] == current_category:
-        print doc["water_footprint_global_average"]["blue"], doc["water_footprint_global_average"]["green"], doc["water_footprint_global_average"]["grey"]
         if doc["water_footprint_global_average"]["blue"]:
             current_category_acc["wf_acc"]["blue"].append(doc["water_footprint_global_average"]["blue"])
         if doc["water_footprint_global_average"]["green"]:
@@ -63,11 +62,6 @@ for doc in collection.find():
         else:
             current_category_acc["global_wf"]["grey"] = None
 
-        print "avg"
-        print current_category_acc["global_wf"]["blue"], current_category_acc["global_wf"]["green"], current_category_acc["global_wf"]["grey"]
-        print doc["product_category"]
-        print "values"
-
         for country, wf in current_category_acc["country_acc"].iteritems():
             wf_types = {"blue": None, "green": None, "grey": None}
             for fp_type in ["blue", "green", "grey"]:
@@ -89,6 +83,7 @@ for doc in collection.find():
         #     "countries": current_category_acc["countries"]
         # })
 
+        # Reset the current category object
         current_category = doc["product_category"]
         current_category_acc = {
             "product": current_category, "global_wf": {"blue": 0, "green": 0, "grey": 0},
@@ -96,8 +91,6 @@ for doc in collection.find():
             "country_acc": {},
             "countries": []
         }
-
-        print doc["water_footprint_global_average"]["blue"], doc["water_footprint_global_average"]["green"], doc["water_footprint_global_average"]["grey"]
 
         if doc["water_footprint_global_average"]["blue"]:
             current_category_acc["wf_acc"]["blue"].append(doc["water_footprint_global_average"]["blue"])
@@ -125,5 +118,40 @@ for doc in collection.find():
                         country["water_footprint_country_average"]["grey"]
                     )
 
+        # Peppermint is our last ingredient so we have to add this to our result manually
+        if current_category_acc["product"] == "Peppermint":
+            if current_category_acc["wf_acc"]["blue"]:
+                current_category_acc["global_wf"]["blue"] = numpy.mean(current_category_acc["wf_acc"]["blue"])
+            else:
+                current_category_acc["global_wf"]["blue"] = None
+            if current_category_acc["wf_acc"]["green"]:
+                current_category_acc["global_wf"]["green"] = numpy.mean(current_category_acc["wf_acc"]["green"])
+            else:
+                current_category_acc["global_wf"]["green"] = None
+            if current_category_acc["wf_acc"]["grey"]:
+                current_category_acc["global_wf"]["grey"] = numpy.mean(current_category_acc["wf_acc"]["grey"])
+            else:
+                current_category_acc["global_wf"]["grey"] = None
+
+            for country, wf in current_category_acc["country_acc"].iteritems():
+                wf_types = {"blue": None, "green": None, "grey": None}
+                for fp_type in ["blue", "green", "grey"]:
+                    if wf[fp_type]:
+                        wf_types[fp_type] = numpy.mean(wf[fp_type])
+
+                current_category_acc["countries"].append({
+                    "country": country,
+                    "wf_country_average": {
+                        "blue": wf_types["blue"],
+                        "green": wf_types["green"],
+                        "grey": wf_types["grey"]
+                    }
+                })
+            aggregated_result.append(current_category_acc)
+
+
 print len(aggregated_result)
-print aggregated_result[0]["product"], aggregated_result[0]["global_wf"]
+
+for ingredient in aggregated_result:
+    print ingredient["product"]
+    print ingredient["global_wf"]
