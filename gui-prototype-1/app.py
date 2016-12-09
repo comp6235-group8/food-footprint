fb26e8739d726e10e0b65afbac5ceecebc3b4b7f
Ronald Gualan <rmgs1u16@soton.ac.uk>
2016-12-09 12:17:58 +0000

    Modification of web services according to the new schema of databases
    Modified services:
    /data/recipies
    /data/ingredients
    New services:
    /data/recipe/ingredients/<recipeName>
    /data/ingredient/waterfootprint/<ingredientName>

--- gui-prototype-1/app.py
+++ gui-prototype-1/app.py
@@ -10,8 +10,10 @@ app = Flask(__name__)
 MONGODB_HOST = 'localhost'
 MONGODB_PORT = 27017
 DBS_NAME = 'water_footprint'
-COLLECTION_RECIPIES = 'recipies'
-COLLECTION_CROP = 'crop_products'
+#COLLECTION_RECIPES = 'recipies'
+COLLECTION_RECIPES = 'recipes_2'
+COLLECTION_CROP = 'crop_products_by_category'
+COLLECTION_INGREDIENT_TO_WATERFP = 'ingredients_to_recipes'
 #FIELDS = {'school_state': True, 'resource_type': True, 'poverty_level': True, 'date_posted': True, 'total_donations': True, '_id': False}
 
 
@@ -23,10 +25,10 @@ def index():
 @app.route("/data/recipies")
 def data_recipies():
     connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
-    collection = connection[DBS_NAME][COLLECTION_RECIPIES]
+    collection = connection[DBS_NAME][COLLECTION_RECIPES]
 
     recipies = collection.distinct("recipeName")
-    recipies = sorted(recipies)
+    recipies = sorted(recipies) 
     print "Number of distinct recipies:",len(recipies)
 
     json_resp = json.dumps(recipies, default=json_util.default)
@@ -36,10 +38,12 @@ def data_recipies():
 @app.route("/data/ingredients")
 def data_ingredients():
     connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
-    collection = connection[DBS_NAME][COLLECTION_RECIPIES]
+    #collection = connection[DBS_NAME][COLLECTION_RECIPIES]
+    collection = connection[DBS_NAME][COLLECTION_INGREDIENT_TO_WATERFP]
 
-    ingredients = collection.distinct("ingredients")
-    ingredients = sorted(ingredients)
+    #ingredients = collection.distinct("ingredients")
+    ingredients = collection.distinct("ingredient")
+    ingredients = sorted(ingredients) 
     print "Number of distinct ingredients:",len(ingredients)
 
     json_resp = json.dumps(ingredients, default=json_util.default)
@@ -63,6 +67,48 @@ def data_wftest():
     connection.close()
     return json_resp
 
+# Returns an array with the ingredients of a recipe
+@app.route("/data/recipe/ingredients/<recipeName>")
+def data_ingredients_per_recipe(recipeName):
+    connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
+    collection = connection[DBS_NAME][COLLECTION_RECIPES]
+
+    print "Processing:", recipeName
+    recipe = collection.find_one({"recipeName":recipeName})
+
+    ingredients = []
+
+    if recipe:
+        ingredients = recipe['ingredients']
+
+    print "Ingredients:", ingredients
+
+    json_resp = json.dumps(ingredients, default=json_util.default)
+    connection.close()
+    return json_resp
+
+# Returns the water footprint associated to an ingredient/product
+@app.route("/data/ingredient/waterfootprint/<ingredientName>")
+def data_waterfootprint_per_ingredient(ingredientName):
+    connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
+    collection = connection[DBS_NAME][COLLECTION_INGREDIENT_TO_WATERFP]
+    collection2 = connection[DBS_NAME][COLLECTION_CROP]
+
+    print "Processing:", ingredientName
+    ing_wf = collection.find_one({"ingredient":ingredientName})
+
+    waterfootprint = {}
+
+    if ing_wf:
+        wf = collection2.find_one({"product_category":ing_wf['product_category'], "product":ing_wf["product"]})
+        if wf:
+            waterfootprint = wf
+
+    print "Water footprint:", waterfootprint['water_footprint_global_average']
+
+    json_resp = json.dumps(waterfootprint, default=json_util.default)
+    connection.close()
+    return json_resp
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000,debug=True)
