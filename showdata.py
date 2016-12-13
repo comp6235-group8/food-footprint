2b41ec1f2ae9c3cc238dee7f882bb2716b69fca1
Jing Wang <junewang.cz@gmail.com>
2016-12-13 11:59:43 +0000

    improve cleaning data
    add regex expression

--- showdata.py
+++ showdata.py
@@ -1,8 +1,11 @@
+# -*- coding: utf-8 -*-
 from pymongo import MongoClient
 import requests
 import re
 import json
 import numpy
+
+
 client = MongoClient('mongodb://localhost:27017')
 db = client.water_footprint
 collection1 = db.water_footprint
@@ -27,28 +30,37 @@ def show_data():
     for i in collection2.find():
         print i
 
-#clean data
+
+    #clean data
 def fun_translate(collection,part):
     result=[""];
     print collection;
-    for i in collection.find().limit(100):
-            i[part] = re.sub(r"&", "@", i[part])
-        # for j in i[part]:
-            url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q="+i[part];
+    for i in collection.find().limit(300):
+            i[part] = re.sub(r"&", "@12", i[part])
+            num = re.sub(r'\{.*?\}', "", i[part])
+            num = re.sub(r'\(.*?\)', "", num)
+            num = re.sub(r'\<g.*?g\>', "", num)
+            num = re.sub(r'\\m/.*?\\m/', "", num)
+            num = re.sub(r'</br>', "", num)
+            num = re.sub(r'//', "", num)
+            num = re.sub(r'\<span\>.*?\<\/span\>', "", num)
+            num = re.sub(r'\<em\>.*?\<\/em\>', "", num)
+            # for j in i[part]:
+            url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q="+num;
             header = {
                 'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:46.0) Gecko/20100101 Firefox/46.0',
                 'Content-Type': 'application/x-www-form-urlencoded',
                 'Connection': 'Keep-Alive',
                 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
                 }
-            r=requests.get(url, headers=header).text
+            r=requests.get(url,headers=header).text
             array=r.split('",');
-            result_one=array[0][4:]
-            num = re.sub('[!\"\\\]', "", result_one)
-            num = re.sub(r"w/", "", num)
-            num = re.sub(r"@", "&", num)
+            num=array[0][4:]
+            num = re.sub(r"@12", "&", num)
+            num = re.sub(r"’", "'", num)
             print num
-            collection.update({"_id": i['_id']},{'$set': {"recipeName":num}})
+            num = re.sub(r'[^a-zA-Z   ‎&0-9\/’\'\‘\’\"\"\“\”,΄.&Øøłướịççảáåỏốôòōöởíıïäàóšñéèëâêêñńûúùüůšæ%ğÁÉ++£«»è ×—–-]', "",num)
+            print num
             result.append(num)
     print result
     return result
@@ -58,3 +70,4 @@ def savetxt(filename,x):
 
 result=fun_translate(collection2,"recipeName")
 savetxt("ingredient",result)
+
