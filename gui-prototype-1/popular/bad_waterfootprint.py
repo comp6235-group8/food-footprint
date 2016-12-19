4250cb9e1136b142ea89976abe47c27b4a7df50d
xin <xz5n15@soton.ac.uk>
2016-12-19 22:09:03 +0000

    deleted wrong folder

--- gui-prototype-1/popular/bad_waterfootprint.py
+++
@@ -1,7 +0,0 @@
-import json as js
-def bad_waterfootprint():
-    with open("popular/dishes.json") as jsonfile:
-        json_data = js.load(jsonfile)
-        print (json_data)
-
-bad_waterfootprint()
