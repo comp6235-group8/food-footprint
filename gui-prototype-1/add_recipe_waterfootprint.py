b4a89c782ad913e42b22092b49beac71027d60d5
Jing Wang <junewang.cz@gmail.com>
2016-12-20 22:32:19 +0000

    add water footprint feature into recipes_2

---
+++ gui-prototype-1/add_recipe_waterfootprint.py
@@ -0,0 +1,15 @@
+import app
+import json
+from pymongo import MongoClient
+def update():
+    connection = MongoClient(app.MONGODB_HOST, app.MONGODB_PORT)
+    collection = connection[app.DBS_NAME][app.COLLECTION_RECIPES]
+    for i in collection.find({}).skip(273360):
+        print(i['recipeName'])
+        ingredients = i['ingredients']
+        delimiter = ','
+        str = delimiter.join(ingredients)
+        water = json.loads(app.recipe_waterfootprint(str))
+        water["total"] = float(water["blue"])+float(water["green"])+float(water["grey"])
+        collection.update({'_id': i['_id']}, {'$set':{'water_footprint': water}})
+update()
