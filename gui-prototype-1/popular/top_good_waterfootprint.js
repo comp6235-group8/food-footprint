463607c18196cee35f0c6e9c8d538485af60c677
xin <xz5n15@soton.ac.uk>
2016-12-25 09:06:18 +0000

    feat/ingredients

---
+++ gui-prototype-1/popular/top_good_waterfootprint.js
@@ -0,0 +1,109 @@
+/**
+ * Created by Administrator on 12/20/2016.
+ */
+function good() {
+    var margin = {top: 20, right: 20, bottom: 150, left: 40},
+        width = 960 - margin.left - margin.right,
+        height = 560 - margin.top - margin.bottom;
+
+    var x0 = d3.scale.ordinal()
+        .rangeRoundBands([0, width], .1);
+
+    var x1 = d3.scale.ordinal();
+
+    var y = d3.scale.linear()
+        .range([height, 0]);
+
+    var color = d3.scale.ordinal()
+        .range(["#6495ed", "#3cb371", "#a9a9a9"]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x0)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .ticks(20)
+        .tickFormat(d3.format(".2s"));
+
+    var svg = d3.select("#good").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + height + "," + margin.top + ")");
+
+    d3.json("good_crop.json", function(error, data) {
+        if (error) throw error;
+
+        var ageNames = d3.keys(data[0]).filter(function(key) { return (key !== "product") && (key !== "amount"); });
+
+        data.forEach(function(d) {
+            d.ages = ageNames.map(function(name) { return {name: name, value: +d[name]}; });
+        });
+        console.log(data);
+        x0.domain(data.map(function(d) { return d.product; }));
+        x1.domain(ageNames).rangeRoundBands([0, x0.rangeBand()]);
+        y.domain([0, d3.max(data, function(d) { return d3.max(d.ages, function(d) { return d.value; }); })]);
+
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis)
+            .selectAll("text")
+            .attr("transform", "rotate(-60)")
+            .attr("dx", "-1em")
+            .attr("dy", ".20em")
+            .style("text-anchor", "end")
+            .style("font-size", "10px");
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("Waterfootprint");
+
+        var state = svg.selectAll(".state")
+            .data(data)
+            .enter().append("g")
+            .attr("class", "state")
+            .attr("transform", function(d) { return "translate(" + x0(d.product) + ",0)"; });
+
+        state.selectAll("rect")
+            .data(function(d) { return d.ages; })
+            .enter().append("rect")
+            .attr("width", x1.rangeBand())
+            .attr("x", function(d) { return x1(d.name); })
+            .attr("y", function(d) { return y(d.value); })
+            .attr("height", function(d) { return height - y(d.value); })
+            .style("fill", function(d) { return color(d.name); });
+
+        var legend = svg.selectAll(".legend")
+            .data(ageNames.slice().reverse())
+            .enter().append("g")
+            .attr("class", "legend")
+            .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+        legend.append("rect")
+            .attr("x", width - 18)
+            .attr("width", 18)
+            .attr("height", 18)
+            .style("fill", color);
+
+        legend.append("text")
+            .attr("x", width - 24)
+            .attr("y", 9)
+            .attr("dy", ".35em")
+            .style("text-anchor", "end")
+            .text(function(d) { return d; });
+
+    });
+}
+
+$(document).ready(function() {
+    good();
+})
