7dad3d71afadfcb65894a2613f4f628aee69d20a
xin <xz5n15@soton.ac.uk>
2016-12-31 09:12:35 +0000

    add top crop water footprint in index

--- gui-prototype-1/static/js/good_crop_waterfootprint.js
+++ gui-prototype-1/static/js/good_crop_waterfootprint.js
@@ -1,6 +1,18 @@
 // var margin = {top: 20, right: 20, bottom: 150, left: 40},
 //         width = 960 - margin.left - margin.right,
 //         height = 560 - margin.top - margin.bottom;
+$(document).ready(function() {
+    good_crop();
+ })
+function good_crop() {
+
+
+    var svg = d3.select("#good_crop");
+
+    var margin = {top: 30, right: 20, bottom: 150, left: 40},
+        width = +svg.attr("width") - margin.left - margin.right,
+        height = +svg.attr("height")- margin.top - margin.bottom,
+        g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     var x0 = d3.scale.ordinal()
         .rangeRoundBands([0, width], .1);
@@ -22,37 +34,37 @@
         .orient("left")
         .ticks(20)
         .tickFormat(d3.format(".2s"));
-
-    // var svg = d3.select("#bad_crop")
-    //     .attr("width", width + margin.left + margin.right)
-    //     .attr("height", height + margin.top + margin.bottom)
-    //     .append("g")
-    //     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    var svg = d3.select("#good_crop"),
-        margin = {top: 20, right: 20, bottom: 150, left: 40},
-        width = +svg.attr("width") - margin.left - margin.right,
-        height = +svg.attr("height") - margin.top - margin.bottom,
-        g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    d3.json("good_crop.json", function(error, data) {
+    var constant = 50;
+    d3.json("../../data/good_crop.json", function (error, data) {
         if (error) throw error;
 
-        var types = d3.keys(data[0]).filter(function(key) { return (key !== "product") && (key !== "amount"); });
+        var types = d3.keys(data[0]).filter(function (key) {
+            return (key !== "product") && (key !== "amount");
+        });
 
-        data.forEach(function(d) {
-            d.waterfootprint = types.map(function(name) { return {name: name, value: +d[name]}; });
+        data.forEach(function (d) {
+            d.waterfootprint = types.map(function (name) {
+                console.log(+d[name]);
+                return {name: name, value: +d[name]};
+            });
         });
         console.log(data);
-        x0.domain(data.map(function(d) { return d.product; }));
+        x0.domain(data.map(function (d) {
+            return d.product;
+        }));
         x1.domain(types).rangeRoundBands([0, x0.rangeBand()]);
-        y.domain([0, d3.max(data, function(d) { return d3.max(d.waterfootprint, function(d) { return d.value; }); })]);
+        y.domain([0, d3.max(data, function (d) {
+            return d3.max(d.waterfootprint, function (d) {
+                return d.value;
+            });
+        })]);
 
         svg.append("g")
             .attr("class", "x axis")
-            .attr("transform", "translate(0," + height + ")")
+            .attr("transform", "translate("+constant+"," + height + ")")
             .call(xAxis)
             .selectAll("text")
-            .attr("transform", "rotate(-60)")
+            .attr("transform", "rotate(-80)")
             .attr("dx", "-1em")
             .attr("dy", ".20em")
             .style("text-anchor", "end")
@@ -61,6 +73,7 @@
 
         svg.append("g")
             .attr("class", "y axis")
+            .attr("transform", "translate("+constant+",0)")
             .call(yAxis)
             .append("text")
             .attr("transform", "rotate(-90)")
@@ -73,33 +86,48 @@
             .data(data)
             .enter().append("g")
             .attr("class", "state")
-            .attr("transform", function(d) { return "translate(" + x0(d.product) + ",0)"; });
+            .attr("transform", function (d) {
+                var length = +x0(d.product) + constant;
+                return "translate("+length+",0)";
+            });
 
         state.selectAll("rect")
-            .data(function(d) { return d.waterfootprint; })
+            .data(function (d) {
+                return d.waterfootprint;
+            })
             .enter().append("rect")
             .attr("width", x1.rangeBand())
-            .attr("x", function(d) { return x1(d.name); })
-            .attr("y", function(d) { return y(d.value); })
-            .attr("height", function(d) { return height - y(d.value); })
-            .style("fill", function(d) { return color(d.name); });
-
-        var _p3 = d3.format(".1f"); // save one number after point
-        var texts = state.selectAll(".MyText")
-            .data(function (d) {return d.waterfootprint;})
-            .enter()
-            .append("text")
             .attr("x", function (d) {
-                return x1(d.name) - 10;
+                return x1(d.name);
             })
             .attr("y", function (d) {
-                return y(d.value) - 25;
+                return y(d.value);
             })
-            .attr("dx", 10)
-            .attr("dy", 20)
-            .text(function (d) {
-                return _p3(d.value);
+            .attr("height", function (d) {
+                return height - y(d.value);
+            })
+            .style("fill", function (d) {
+                return color(d.name);
             });
+
+        // var _p3 = d3.format(".1f"); // save one number after point
+        // var texts = state.selectAll(".MyText")
+        //     .data(function (d) {
+        //         return d.waterfootprint;
+        //     })
+        //     .enter()
+        //     .append("text")
+        //     .attr("x", function (d) {
+        //         return x1(d.name) - 10;
+        //     })
+        //     .attr("y", function (d) {
+        //         return y(d.value) - 25;
+        //     })
+        //     .attr("dx", 10)
+        //     .attr("dy", 20)
+        //     .text(function (d) {
+        //         return _p3(d.value);
+        //     });
 
         var legend = svg.selectAll(".legend")
             .data(types.slice().reverse())
@@ -120,6 +148,9 @@
             .attr("y", 9)
             .attr("dy", ".35em")
             .style("text-anchor", "end")
-            .text(function(d) { return d; });
+            .text(function (d) {
+                return d;
+            });
 
     });
+}
